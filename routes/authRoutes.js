5b7723d8180250c9da68afaffa38d1dd8679daa3
Crackvignoule <88386090+Crackvignoule@users.noreply.github.com>
2024-07-12 11:22:46 +0000

    WIP

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// EXISTING ROUTES
+// Use http://auth/api/auth/generateToken to generate a token
+// Use http://auth/api/auth/verifyToken to verify a token
+
+// Use data service to write/read to/from the database
+
+// TO WRITE
+// Login Route
+
+
+module.exports = router;
--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -8,4 +8,27 @@ router.get('/weather', asyncHandler(async (req, res) => {
   res.json(response.data);
 }));
 
+// /users, get a batch of users
+router.get('/users', asyncHandler(async (req, res) => {
+  const response = await axios.get('http://users/users');
+  res.json(response.data);
+}));
+
+// /users/me get the current user
+router.get('/users/me', asyncHandler(async (req, res) => {
+  const token = req.headers.authorization;
+  const response = await axios.get('http://auth/me', {
+    headers: {
+      Authorization: token
+    }
+  });
+  res.json(response.data);
+}));
+
+// GET /rooms get list of rooms
+// POST /rooms create a new room
+// GET /rooms/:id get messages history of a room
+// POST /rooms/:id send a message to a room
+
+
 module.exports = router;
